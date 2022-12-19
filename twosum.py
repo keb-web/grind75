e2baaf251734b6af95bc6b40924dae7066039708
keb-web <kebinhleh@gmail.com>
2022-12-19 17:37:49 -0600

    twosum

---
+++ twosum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for x in range(nums):
+            if target - x 
+
+
+
+nums = [2,7,11,15]
+target = 9
+s = Solution();
+print(s.twoSum(nums, target))
